93f759bc1b277ba9a9fff849457c31dd35a9b731
Tim Popham <popham@uw.edu>
2019-02-13 20:35:48 -0800

    Convert lo bytes to unsigned

--- src/type.js
+++ src/type.js
@@ -1,11 +1,12 @@
 /* @flow */
 
 type i32 = number;
+type u32 = number;
 
-export opaque type Int64: [ i32, i32 ] = [ i32, i32 ];
+export opaque type Int64: [ i32, u32 ] = [ i32, u32 ];
 
 export function inject(hi: i32, lo: i32): Int64 { // eslint-disable-line no-shadow
-  return [ hi, lo ];
+  return [ hi, lo >>> 0 ];
 }
 
 export function hi(int64: Int64): i32 {
